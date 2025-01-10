216d8fe19a59be88f629ffebebebe146874d837a
su1gen <prybylovychav@gmail.com>
2025-01-10 14:48:08 +0200

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import { useEffect, useState } from 'react'
+import { Suspense, useEffect, useState } from 'react'
 import { useSearchParams } from 'next/navigation'
 
-export default function Home() {
-  const searchParams = useSearchParams()
+
+function SearchParamsBlock() {
   const [fileName, setFileName] = useState('')
+  const searchParams = useSearchParams()
 
   useEffect(() => {
     // Получаем имя файла из URL
@@ -21,21 +22,26 @@ export default function Home() {
     }
   }, [searchParams])
 
+  return <div className="bg-green-100 p-4 rounded-lg">
+    <h2 className="font-semibold">Получен файл:</h2>
+    <p className="mt-2">{fileName}</p>
+  </div>
+
+}
+
+
+export default function Home() {
+
+
   return (
     <main className="min-h-screen p-4">
       <div className="max-w-md mx-auto mt-10">
         <h1 className="text-2xl font-bold mb-4">File Sharing PWA</h1>
 
-        {fileName ? (
-          <div className="bg-green-100 p-4 rounded-lg">
-            <h2 className="font-semibold">Получен файл:</h2>
-            <p className="mt-2">{fileName}</p>
-          </div>
-        ) : (
-          <div className="bg-gray-100 p-4 rounded-lg">
-            <p>Поделитесь файлом, чтобы увидеть его здесь</p>
-          </div>
-        )}
+        <Suspense>
+          <SearchParamsBlock/>
+        </Suspense>
+
       </div>
     </main>
   )
