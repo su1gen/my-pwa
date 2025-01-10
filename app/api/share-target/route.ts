04587c6c325f9d03e7f54ddca57e452c36100c22
su1gen <prybylovychav@gmail.com>
2025-01-10 14:30:46 +0200

    Update

--- app/api/share-target/route.ts
+++ app/api/share-target/route.ts
@@ -5,16 +5,7 @@ export async function POST(request: Request) {
     const formData = await request.formData()
     const file = formData.get('file') as File
 
-    if (!file) {
-      // Создаем URL с использованием URL constructor
-      const errorUrl = new URL('/', request.url)
-      errorUrl.searchParams.set('error', 'no-file')
-      return NextResponse.redirect(errorUrl.toString())
-    }
-
-    // Создаем корректный URL для редиректа
-
-    return NextResponse.redirect(`/file?name=${file.name}`)
+    return NextResponse.redirect(`https://my-pwa-phi.vercel.app//file?name=${file.name}`)
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
     // В случае ошибки также используем URL constructor
