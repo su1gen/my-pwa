a88554a38f3503fb0790412adb685091ae9e1d70
su1gen <prybylovychav@gmail.com>
2025-01-13 18:50:46 +0200

    Update

--- worker/index.ts
+++ worker/index.ts
@@ -6,26 +6,34 @@
 
 self.addEventListener('fetch', event => {
 
-  alert("fetch")
+  if (event.request.clone().method === 'POST') {
+    const url = new URL(event.request.clone().url);
+    console.log('URL');
+    console.log(url);
+    if (event.request.clone().url.endsWith('/api/share-target') || url.pathname == '/api/share-target') {
+      event.respondWith(Response.redirect('./'));
 
-  // const url = new URL(event.request.clone().url);
-  // If this is an incoming POST request for the
-  // registered "action" URL, respond to it.
-  if (event.request.method === 'POST' /*&& url.pathname === '/qtransfer-p/send.php'*/) {
-    alert("post")
-    // event.respondWith(Response.redirect('/qtransfer-p/send.php'));
-    // event.waitUntil((async () => {
-    //   alert("1");
-    //   const data = await event.request.clone().formData();
-    //   alert("2");
-    //   alert(data);
-    //   const client = await self.clients.get(event.resultingClientId || event.clientId);
-    //   alert("3");
-    //   const file = data.get("fileToUpload");
-    //   alert("file", file);
-    //   client.postMessage({ file, action: 'load-file' });
-    // })());
-    // alert("ok");
-    return 0;
+      event.waitUntil(async function () {
+        const data = await event.request.formData();
+        console.log(data);
+        const client = await self.clients.get(event.resultingClientId);
+        console.log(client);
+        const file = data.get('file');
+        client.postMessage({ file });
+      }());
+    } else {
+      // do other non related stuff
+    }
   }
+});
+
+navigator.serviceWorker.addEventListener('message', event => {
+
+  const file = event.data.file;
+  const img  = new Image();
+  const url = URL.createObjectURL(file);
+  img.src = url;
+  document.body.append(img);
+  document.body.append(url);
+  document.body.append('<h1>test</h1>');
 });
